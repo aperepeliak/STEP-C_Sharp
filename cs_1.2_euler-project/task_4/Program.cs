33012f4a409884d20f3bd1a48ad973f3ab6895a6
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-30 13:49:40 +0300

    start task 4

---
+++ cs_1.2_euler-project/task_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the largest palindrome made from the product of two 3-digit numbers.
+
+        }
+    }
+}
