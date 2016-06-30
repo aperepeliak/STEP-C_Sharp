76ad1a065bcd33dec1f3a705028d2789ac23b912
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-30 15:01:43 +0300

    start task 5 project

---
+++ cs_1.2_euler-project/task_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+        }
+    }
+}
