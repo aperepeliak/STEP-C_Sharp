a11ba397baa5e98f47bff309f9bf3cdde48fc956
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-30 15:11:43 +0300

    add task 5 solution

--- cs_1.2_euler-project/task_5/Program.cs
+++ cs_1.2_euler-project/task_5/Program.cs
@@ -11,7 +11,18 @@ namespace task_5
         static void Main(string[] args)
         {
             // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-
+            int result = 20;
+            while (result % 2 != 0 || result % 3 != 0 || result % 4 != 0 ||
+                   result % 5 != 0 || result % 6 != 0 || result % 7 != 0 ||
+                   result % 8 != 0 || result % 9 != 0 || result % 10 != 0 ||
+                   result % 11 != 0 || result % 12 != 0 || result % 13 != 0 ||
+                   result % 14 != 0 || result % 15 != 0 || result % 16 != 0 ||
+                   result % 17 != 0 || result % 18 != 0 || result % 19 != 0 ||
+                   result % 20 != 0)
+            {
+                result += 20;
+            }
+            Console.WriteLine("Result : {0}", result);
         }
     }
 }
