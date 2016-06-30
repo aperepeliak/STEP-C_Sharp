1d640e111de61dd3cc92ff5f7f7a0cd0289e6c70
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-30 11:33:44 +0300

    solve task 2

--- cs_1.2_euler-project/task_2/Program.cs
+++ cs_1.2_euler-project/task_2/Program.cs
@@ -10,7 +10,37 @@ namespace task_2
     {
         static void Main(string[] args)
         {
+            // By starting with 1 and 2, the first 10 terms will be:  1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
             // By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
+            int MAX_FIBO = 4000000;
+            List<int> fibo = new List<int>();
+
+            int a = 1, b = 1, c = 1;
+            while (c < MAX_FIBO)
+            {
+                fibo.Add(b);
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            Console.WriteLine("fibo sequence up to 4 000 000: ");
+            for (int i = 0; i < fibo.Count; i++)
+            {
+                Console.WriteLine("fibo[{0}] = {1}", i, fibo[i]);
+            }
+            Console.WriteLine("-------------------------------");
+
+            int fiboSumEven = 0;
+            for (int i = 0; i < fibo.Count; i++)
+            {
+                if ((fibo[i] % 2) == 0)
+                {
+                    fiboSumEven += fibo[i];
+                }
+            }
+            Console.WriteLine("Sum of the even-valued terms : {0}", fiboSumEven);
         }
     }
 }
