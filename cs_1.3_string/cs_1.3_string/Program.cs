0768fdc374a4c54f4de151e4e1abf35aadf18363
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-30 16:38:28 +0300

    start point b

--- cs_1.3_string/cs_1.3_string/Program.cs
+++ cs_1.3_string/cs_1.3_string/Program.cs
@@ -10,6 +10,22 @@ namespace cs_1._3_string
     {
         static void Main(string[] args)
         {
+            string userString;
+            Console.Write("Введите строку -> ");
+            userString = Console.ReadLine();
+            Console.WriteLine("Вы ввели : {0}", userString);
+
+            // a)
+            Console.WriteLine("а) Количество символов в строке: {0}", userString.Length);
+
+            // б)
+            char mostOccurSymbol;
+            int numOccur;
+
+            for (int i = 0; i < userString.Length; i++)
+            {
+                int
+            }
         }
     }
 }
