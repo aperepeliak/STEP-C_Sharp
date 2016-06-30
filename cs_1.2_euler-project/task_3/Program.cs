289abb85806bd1193c94b9ed819d3272a7e73f04
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-30 12:33:35 +0300

    add task 3 solution

---
+++ cs_1.2_euler-project/task_3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // What is the largest prime factor of the number 600851475143 ?
+            const long INIT_NUM = 600851475143;
+            long number = INIT_NUM;
+            int curDiv = 2;
+            int maxDiv = 0;
+            while (curDiv < (number / 2))
+            {
+                if ((number % curDiv) == 0)
+                {
+                    if (curDiv > maxDiv)
+                    {
+                        maxDiv = curDiv;
+                    }
+                    number = number / curDiv;
+                    curDiv = 2;
+                }
+                else
+                {
+                    curDiv++;
+                }
+            }
+            Console.WriteLine("The largest prime factor of the number {0} is : {1}", INIT_NUM, maxDiv);
+        }
+    }
+}
