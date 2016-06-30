0411e7a75dd710632db1a58a33698b74da1484a7
Andrii Perepeliak <aperepelyak@gmail.com>
2016-06-30 14:22:39 +0300

    add solution to task 4

--- cs_1.2_euler-project/task_4/Program.cs
+++ cs_1.2_euler-project/task_4/Program.cs
@@ -11,7 +11,40 @@ namespace task_4
         static void Main(string[] args)
         {
             // Find the largest palindrome made from the product of two 3-digit numbers.
+            int largestPoLiThreeDigits = 0;
+            for (int i = 999; i > 100; i--)
+            {
+                for (int j = 999; j > 100; j--)
+                {
+                    if (isPolindrome(i * j))
+                    {
+                        largestPoLiThreeDigits = ((i * j) > largestPoLiThreeDigits) ? (i * j) : largestPoLiThreeDigits;
+                    }
+                }
+            }
 
+            if (largestPoLiThreeDigits != 0)
+            {
+                Console.WriteLine("the largest palindrome made from the product of two 3-digit numbers : {0}", largestPoLiThreeDigits);
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+        }
+
+        static bool isPolindrome(int n)
+        {
+            string NumToString = n.ToString();
+            char[] NumReverse = NumToString.Reverse().ToArray();
+            for (int i = 0; i < NumToString.Length; i++)
+            {
+                if (NumToString[i] != NumReverse[i])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
