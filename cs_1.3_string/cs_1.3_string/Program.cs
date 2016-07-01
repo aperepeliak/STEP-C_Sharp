ed84d648e7bc3bd08e3edd48bf9fd60f69d7c8f9
Andrii Perepeliak <aperepelyak@gmail.com>
2016-07-01 07:48:16 +0300

    add subtask b)

--- cs_1.3_string/cs_1.3_string/Program.cs
+++ cs_1.3_string/cs_1.3_string/Program.cs
@@ -14,18 +14,36 @@ namespace cs_1._3_string
             Console.Write("Введите строку -> ");
             userString = Console.ReadLine();
             Console.WriteLine("Вы ввели : {0}", userString);
+            Console.WriteLine("-----");
 
             // a)
             Console.WriteLine("а) Количество символов в строке: {0}", userString.Length);
+            Console.WriteLine("-----");
+            //-----------------------------------------
 
             // б)
-            char mostOccurSymbol;
-            int numOccur;
+            char mostOccurSymbol = '\0';
+            int numOccur = 0;
 
             for (int i = 0; i < userString.Length; i++)
             {
-                int
+                int counter = 0;
+                for (int j = i; j < userString.Length; j++)
+                {
+                    if (userString[i] == userString[j])
+                    {
+                        counter++;
+                    }
+                }
+                if (numOccur < counter)
+                {
+                    numOccur = counter;
+                    mostOccurSymbol = userString[i];
+                }
+                counter = 0;
             }
+            Console.WriteLine("Самый повторяющийся символ в строке : " + mostOccurSymbol + " он встречается {0} раз(-а)", numOccur);
+            Console.WriteLine("-----");
         }
     }
 }
