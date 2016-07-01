d342151a3a916bc967b6343328357792ac92402e
Andrii Perepeliak <aperepelyak@gmail.com>
2016-07-01 09:38:17 +0300

    finish task 1

--- cs_1.3_string/cs_1.3_string/Program.cs
+++ cs_1.3_string/cs_1.3_string/Program.cs
@@ -14,13 +14,18 @@ namespace cs_1._3_string
             Console.Write("Введите строку -> ");
             userString = Console.ReadLine();
             Console.WriteLine("Вы ввели : {0}", userString);
+            StringAnalyzer(userString);
             Console.WriteLine("-----");
 
+            
+
+        }
+
+        static void StringAnalyzer(string userString)
+        {
             // a)
             Console.WriteLine("а) Количество символов в строке: {0}", userString.Length);
-            Console.WriteLine("-----");
-            //-----------------------------------------
-
+           
             // б)
             char mostOccurSymbol = '\0';
             int numOccur = 0;
@@ -42,8 +47,36 @@ namespace cs_1._3_string
                 }
                 counter = 0;
             }
-            Console.WriteLine("Самый повторяющийся символ в строке : " + mostOccurSymbol + " он встречается {0} раз(-а)", numOccur);
-            Console.WriteLine("-----");
+            Console.WriteLine("б) Самый повторяющийся символ в строке : " + mostOccurSymbol + " он встречается {0} раз(-а)", numOccur);
+            
+
+            // в)
+            Console.WriteLine("в) Количество цифр в строке : {0}", userString.Count(char.IsDigit));
+           
+            // г)
+            Console.WriteLine("г) Количество букв в верхнем регистре : {0}", userString.Count(char.IsUpper));
+            
+            // д)
+            Console.WriteLine("д) Количество букв в нижнем регистре : {0}", userString.Count(char.IsLower));
+            
+            // e)
+            int numSentences = (userString.Split('.').Length - 1) + (userString.Split('!').Length - 1) + (userString.Split('?').Length - 1);
+            Console.WriteLine("е) Количество предложений : {0}", numSentences);
+            
+            // ж) 
+            string[] words = userString.Split(' ');
+            int counterNumbers = 0;
+            foreach (var word in words)
+            {
+                double tmp = 0;
+                bool isNumeric = double.TryParse(word, out tmp);
+                if (isNumeric)
+                {
+                    counterNumbers++;
+                }
+            }
+            Console.WriteLine("ж) Количество чисел в строке : {0}", counterNumbers);
+            
         }
     }
 }
