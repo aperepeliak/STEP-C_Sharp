565c881cd3eb217b3ab449695104ef2ebec295e1
Andrii Perepeliak <aperepelyak@gmail.com>
2016-07-07 15:19:08 +0300

    2.3 project init

---
+++ cs_2.3_operators_overload/cs_2.3_operators_overload/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_2._3_operators_overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
