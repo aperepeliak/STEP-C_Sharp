860e6006ffab41eced575800620973cd948f14bb
Andrii Perepeliak <aperepelyak@gmail.com>
2016-07-07 10:04:58 +0300

    init cs_2.2 project

---
+++ cs_2.2_exceptions/cs_2.2_exceptions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_2._2_exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
