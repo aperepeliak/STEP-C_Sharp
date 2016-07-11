316b833cb1ce692d079c18e1bd0d17e6d605dd44
Andrii Perepeliak <aperepelyak@gmail.com>
2016-07-11 21:19:56 +0300

    start project 3.2

---
+++ cs_3.2_inheritance/cs_3.2_inheritance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_3._2_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
