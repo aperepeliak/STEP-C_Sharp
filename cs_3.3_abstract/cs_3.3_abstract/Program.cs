0bd3aa83be43378c96fd8dd2877b28816e7afd18
Andrii Perepeliak <aperepelyak@gmail.com>
2016-07-12 13:41:45 +0300

    add initial code

--- cs_3.3_abstract/cs_3.3_abstract/Program.cs
+++ cs_3.3_abstract/cs_3.3_abstract/Program.cs
@@ -8,8 +8,71 @@ namespace cs_3._3_abstract
 {
     class Program
     {
+
+        abstract class Item
+        {
+            public abstract string Draw();
+        }
+        class A : Item
+        {
+            public override string Draw()
+            {
+                return "A";
+            }
+        }
+        class B : Item
+        {
+            public override string Draw()
+            {
+                return "B";
+            }
+        }
+        class C : Item
+        {
+            public override string Draw()
+            {
+                return "C";
+            }
+        }
+        class Menu
+        {
+            Item[] _arr = new Item[7];
+            public void Add(Item it, int position)
+            {
+                _arr[position] = it;
+            }
+            public void Remove(int position)
+            {
+                _arr[position] = null;
+            }
+            public void Show()
+            {
+                for (int i = 0; i < _arr.Length; i++)
+                {
+                    Console.Write(" " + i);
+                }
+                Console.WriteLine();
+                for (int i = 0; i < _arr.Length; i++)
+                {
+                    Console.Write('|');
+                    if (_arr[i] != null)
+                        Console.Write(_arr[i].Draw());
+                    else Console.Write(" ");
+                }
+                Console.Write("|");
+            }
+        }
         static void Main(string[] args)
         {
+            Menu m = new Menu();
+            m.Add(new A(), 2);
+            m.Add(new A(), 5);
+            m.Add(new A(), 6);
+            m.Add(new B(), 1);
+            m.Add(new B(), 4);
+            m.Add(new C(), 0);
+            m.Show();
         }
+
     }
 }
