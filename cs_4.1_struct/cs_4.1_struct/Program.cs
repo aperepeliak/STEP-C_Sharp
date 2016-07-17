98e915fc4a7ac6b7b39d2329eca06a9a838ac4f1
Andrii Perepeliak <aperepelyak@gmail.com>
2016-07-17 20:23:55 +0300

    add project 4.1_struct

---
+++ cs_4.1_struct/cs_4.1_struct/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_4._1_struct
+{
+    struct Article
+    {
+        public int productID;
+        public string productName;
+        public decimal productPrice;
+    }
+
+    struct Client
+    {
+        public int clientID;
+        public Name clientName;
+        public Address clientAddress;
+        public string clientTelephone;
+        public int ordersHistoryQuantity;
+        public decimal ordersHistorySum;
+
+        public struct Name
+        {
+            public string firstName;
+            public string middleName;
+            public string lastName;
+        }
+
+        public struct Address
+        {
+            public string country;
+            public string city;
+            public string street;
+            public int buildingNumber;
+        }
+    }
+
+    struct RequestItem
+    {
+        public Article product;
+        public int quantity;
+    }
+
+    struct Request
+    {
+        public int requestID;
+        public Client client;
+        public DateTime orderDate;
+        public List<RequestItem> requestItemList;
+        private decimal OrderSum
+        {
+            get
+            {
+                decimal sum = 0;
+                for (int i = 0; i < requestItemList.Count(); i++)
+                {
+                    sum += (requestItemList[i].product.productPrice * requestItemList[i].quantity);
+                }
+                return sum;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
