f0b7d4223d2b83d968055f3206def3322678b69a
Andrii Perepeliak <aperepelyak@gmail.com>
2016-07-18 21:50:56 +0300

    add bug class

---
+++ cs_4.2_events/cs_4.2_events/Bug.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_4._2_events
+{
+    public delegate void DelegateDanger();
+
+    class Bug
+    {
+        public event DelegateDanger danger = null;
+
+        public int x;
+        public int y;
+
+        //public int X { get; set; }
+        //public int Y { get; set; }
+        public string Name { get; set; }
+
+        public void Move()
+        {
+            Random r = new Random();
+            x = r.Next(2);
+            y = r.Next(2);
+        }
+
+        public void Run()
+        {
+            x = x == 0 ? 0 : x-1;
+            y = y == 0 ? 0 : y-1;
+        }
+
+        public void InvokeEvent()
+        {
+            danger.Invoke();
+        }
+
+
+    }
+}
---
+++ cs_4.2_events/cs_4.2_events/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_4._2_events
+{
+    class Program
+    {
+        //static private void Run()
+        //{
+           
+        //}
+
+        static void Main(string[] args)
+        {
+            Bug b = new Bug();
+
+            b.danger += new DelegateDanger(b.Run);
+
+            Console.WriteLine("Bug pos: {0}, {1}", b.x++, b.y++);
+            Console.WriteLine("Bug pos: {0}, {1}", b.x++, b.y++);
+            Console.WriteLine("Bug pos: {0}, {1}", b.x++, b.y++);
+            Console.WriteLine("Bug pos: {0}, {1}", b.x++, b.y++);
+            b.InvokeEvent();
+            b.InvokeEvent();
+            b.InvokeEvent();
+            b.InvokeEvent();
+
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Bug pos: {0}, {1}", b.x++, b.y++);
+            Console.WriteLine("Bug pos: {0}, {1}", b.x++, b.y++);
+        }
+    }
+}
