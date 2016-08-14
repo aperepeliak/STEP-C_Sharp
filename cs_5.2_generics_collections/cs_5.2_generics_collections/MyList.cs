01b09c27ada6e57c53cf89d1625262a3b0b3143e
aperepeliak <aperepelyak@gmail.com>
2016-08-14 13:20:49 +0300

    add indexer, add(), read-only size

---
+++ cs_5.2_generics_collections/cs_5.2_generics_collections/MyList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_5._2_generics_collections
+{
+    class MyList<T>
+    {
+        T[] arr;
+        private int size;
+
+        public MyList()
+        {
+            arr = null;
+            size = 0;
+        }
+
+        // Read-only property for total number of elements
+        public int Size
+        {
+            get { return size; }
+        }
+
+        public void Add(T newElement)
+        {
+            if (size == 0)
+            {
+                size++;
+                arr = new T[size];
+                arr[size - 1] = newElement;
+            }
+            else
+            {
+                size++;
+                T[] tmp = new T[size];
+                for (int i = 0; i < tmp.Length - 1; i++)
+                {
+                    tmp[i] = arr[i];
+                }
+                tmp[size - 1] = newElement;
+                arr = tmp;
+            }
+        }
+
+        // Indexer
+        public T this[int key]
+        {
+            get
+            {
+                return arr[key];
+            }
+            set
+            {
+                arr[key] = value;
+            }
+        }
+
+
+    }
+}
--- cs_5.2_generics_collections/cs_5.2_generics_collections/Program.cs
+++ cs_5.2_generics_collections/cs_5.2_generics_collections/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static System.Console;
+
 namespace cs_5._2_generics_collections
 {
     class Program
     {
         static void Main(string[] args)
         {
+            MyList<int> test = new MyList<int>();
+
+            test.Add(5);
+            test.Add(6);
+            test.Add(1);
+            test.Add(8);
+
+            Write("My List:\t");
+            for (int i = 0; i < test.Size; i++)
+            {
+                Write($"{test[i]} ");
+            }
+
+            Write("\nSize:\t\t");
+            WriteLine($"{test.Size}");
+
         }
     }
 }
