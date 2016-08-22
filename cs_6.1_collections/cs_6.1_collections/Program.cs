516045c4532d34c5783cc91a3da59989a45cfd96
aperepeliak <aperepelyak@gmail.com>
2016-08-22 09:21:35 +0300

    init 6.1 project

---
+++ cs_6.1_collections/cs_6.1_collections/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_6._1_collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
