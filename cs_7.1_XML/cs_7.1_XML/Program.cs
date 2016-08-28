da6884b3cf79d7c1be0cb315079151886231fa5a
aperepeliak <aperepelyak@gmail.com>
2016-08-28 20:02:34 +0300

    project 7.1 init

---
+++ cs_7.1_XML/cs_7.1_XML/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_7._1_XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
