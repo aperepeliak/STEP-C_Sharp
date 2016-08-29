9665e384adbca74e4cab9aba6dc6c6cdce9c0f05
aperepeliak <aperepelyak@gmail.com>
2016-08-29 13:20:38 +0300

    add classes

---
+++ cs_7.2_serialization/cs_7.2_serialization/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_7._2_serialization
+{
+    public enum Color { Black, Green, Red, Navy, Olive, White, Pink };
+
+    public enum Model { BMW, ZAZ, Mercedes, Mazda, VAZ, Nissan, Honda, Subaru, Toyota, VW};
+
+    [Serializable]
+    public class Car
+    {
+        static int ID = 1;
+        public int Number { get; set; }
+        public Color CarColor { get; set; }
+        public Model CarModel { get; set; }
+
+        public Car()
+        {
+            Number = ID++;
+        }
+
+        public Car(Color col, Model mod)
+            : this()
+        {
+            CarColor = col;
+            CarModel = mod;
+        }
+
+    }
+}
---
+++ cs_7.2_serialization/cs_7.2_serialization/CarsLife.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_7._2_serialization
+{
+    [Serializable]
+    public class AllCars
+    {
+        List<Car> availableCars;
+        List<Car> parkedCars;
+        List<Car> brokenCars;
+
+        public AllCars()
+        {
+            availableCars = new List<Car>();
+            parkedCars = new List<Car>();
+            brokenCars = new List<Car>();
+        }
+
+        public void AddCar(Color col, Model mod)
+        {
+            availableCars.Add(new Car(col, mod));
+        }
+
+        public void RemoveCar(int index)
+        {
+            availableCars.RemoveAt(index);
+        }
+
+        public void AddParkedCar(Car carToPark)
+        {
+            parkedCars.Add(carToPark);
+        }
+
+        public void RemoveParkedCar(int index)
+        {
+            parkedCars.RemoveAt(index);
+        }
+
+        public void AddBrokenCar(Car brokenCar)
+        {
+            brokenCars.Add(brokenCar);
+        }
+
+        public void RemoveBrokenCar(int index)
+        {
+            brokenCars.RemoveAt(index);
+        }
+    }
+}
